043178125991d72c50c4f899b7623a516db0eff0
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-12 12:01:26 +0100

    change name of container div and add title manipualting the dom

--- script.js
+++ script.js
@@ -2,5 +2,11 @@
 
 // Create a single div
 
-newDiv = document.createElement("div");
-document.body.append(newDiv)
+containerDiv = document.createElement("div");
+document.body.append(containerDiv)
+
+// set container as ID for main container
+containerDiv.setAttribute("id", "container");
+
+//create one new div in container
+
