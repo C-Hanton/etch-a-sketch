3024687c218b5f6a45a235840db9df325418c589
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-12 12:08:35 +0100

    add for loop to iterate 256 times for 16 x 16

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ containerDiv.setAttribute("id", "container");
 
 //create one new div in container
 
+
+//newDiv = document.createElement("div");
+//containerDiv.append(newDiv);
+
+// iterate 256 times + generate a div for each iteration
+for(let count = 1; count <= 256 ; count ++){
+    //console.log(count) - Check iteration number - interates to 256
+    newDiv = document.createElement("div");
+    containerDiv.append(newDiv);
+}
